bf1728d087ad2c151b1688486fabc69027b8b462
Sergiy Kalinkin <skalinkin@gmail.com>
2022-06-24 19:12:09 -0400

    disable docker to let user enable it when ready

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
 @REM packer build -force .\rocky8.json
-vagrant cloud publish --force --release --version-description "VirtualBox 6.1.34, Vagrant 2.2.19, Docker 20.10.17" skalinkin/rocky-8 2022.175.1 virtualbox ./builds/virtualbox-rocky-8.box
+vagrant cloud publish --force --release --version-description "VirtualBox 6.1.34, Vagrant 2.2.19, Docker 20.10.17" skalinkin/rocky-8 2022.175.2 virtualbox ./builds/virtualbox-rocky-8.box
