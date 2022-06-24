d6fe295086b76cc435cb6024ef69941bcc5fc184
Sergiy Kalinkin <skalinkin@gmail.com>
2022-06-24 00:15:24 -0400

    removing system update to speed up the process

--- publish.bat
+++ publish.bat
@@ -1,1 +1,2 @@
-packer build -force .\rocky8.json
+@REM packer build -force .\rocky8.json
+vagrant cloud publish --force --release --version-description "VirtualBox 6.1.34, Vagrant 2.2.19, Docker 20.10.17" skalinkin/rocky-8 2022.175.0 virtualbox ./builds/virtualbox-rocky-8.box
