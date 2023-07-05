bf05b0a32f4aa0e4fcbc1e18d65b496063b8af0d
Sergiy Kalinkin <skalinkin@gmail.com>
2023-07-05 17:34:01 -0400

    cleanup docker compose installation

--- publish.bat
+++ publish.bat
@@ -1,2 +1,2 @@
 @REM packer build -force .\rocky8.json
-vagrant cloud publish --force --release --version-description "VirtualBox 7.0.4, Vagrant 2.3.4, Docker 20.10.17" skalinkin/rocky-9 2022.351.1 virtualbox ./builds/virtualbox-rocky-9.box
+vagrant cloud publish --force --release --version-description "VirtualBox 7.0.8, Vagrant 2.3.7, Docker 24.0.2" skalinkin/rocky-9 2023.186.0 virtualbox ./builds/virtualbox-rocky-9.box
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,19 +2,6 @@
 
 # Install docker
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-dnf install -y docker-ce-24.0.2 docker-ce-cli-24.0.2 containerd.io-1.7.2
-echo "{" >> /etc/docker/daemon.json
-echo "  \"data-root\": \"/media/persistent-storage/docker-data\"" >> /etc/docker/daemon.json
-echo "}" >> /etc/docker/daemon.json
+dnf install -y docker-ce-24.0.2 docker-ce-cli-24.0.2 containerd.io-1.6.21 docker-compose-plugin-2.6.0 
 
-# Install docker-compose
-curl -s https://api.github.com/repos/docker/compose/releases/tags/v2.18.1 \
-  | grep browser_download_url \
-  | grep docker-compose-linux-x86_64 \
-  | cut -d '"' -f 4 \
-  | wget -qi -
-chmod +x docker-compose-linux-x86_64
-mv docker-compose-linux-x86_64 /bin/docker-compose
-rm -f docker-compose-linux-x86_64.sha256
-/bin/docker-compose version
 usermod -a -G docker vagrant
